5232e9e1e0c208d3476d75396f2b3a8af43ff0eb
Jared Hodgkins <jared.hodgkins@gmail.com>
2023-11-03 14:50:12 -0400

    51. picking a random element

--- 04-state-how-to-change-your-app/src/App.js
+++ 04-state-how-to-change-your-app/src/App.js
@@ -2,17 +2,23 @@ import { useState } from 'react';
 
 import AnimalShow from './AnimalShow';
 
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   return (
     <div className='card'>
       <button onClick={handleClick}>Add Animal</button>
-      <div>Animal Count: {count}</div>
+      <div>{animals}</div>
     </div>
   );
 }
