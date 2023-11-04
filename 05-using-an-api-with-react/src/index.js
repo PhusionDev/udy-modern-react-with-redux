3bd9bd8e0f3cce429ce566433e2e48c4d10fd921
Kavabux <PhusionDev@gmail.com>
2023-11-04 06:26:47 -0400

    66. data fetching cleanup

--- 05-using-an-api-with-react/src/index.js
+++ 05-using-an-api-with-react/src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-// import searchImages from './api';
-
-// searchImages();
-
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(<App />);
