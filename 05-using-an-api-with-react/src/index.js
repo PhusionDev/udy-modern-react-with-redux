83e746bbfc6ca790b6e349132725b03b150424aa
Kavabux <PhusionDev@gmail.com>
2023-11-03 21:40:39 -0400

    64. making an http request

--- 05-using-an-api-with-react/src/index.js
+++ 05-using-an-api-with-react/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+// import searchImages from './api';
+
+// searchImages();
+
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(<App />);
