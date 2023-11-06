f45adf61fa2a94316ed9f584bf6fce05a0ef2e15
Kavabux <PhusionDev@gmail.com>
2023-11-06 06:30:16 -0500

    104. adding a book for real

--- 06-how-to-handle-forms/src/App.js
+++ 06-how-to-handle-forms/src/App.js
@@ -6,11 +6,12 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log('Need to add book with:', title);
+    setBooks([...books, { id: 123, title }]);
   };
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
