b1cfc0528bb2142938971aaa0cc0e114cd1660ef
Jared Hodgkins <jared.hodgkins@gmail.com>
2023-11-07 11:03:04 -0500

    initial commit - start of section 07

---
+++ 07-data-persistence-with-api-requests/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    const newId = Math.round(Math.random() * 10000);
+    setBooks([...books, { id: newId, title }]);
+  };
+
+  const deleteBookById = (id) => {
+    setBooks(books.filter((book) => book.id !== id));
+  };
+
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div className='app'>
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
+
+export default App;
