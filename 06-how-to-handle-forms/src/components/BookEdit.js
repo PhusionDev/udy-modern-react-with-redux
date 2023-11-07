039a899b8c467a58ac235f6f41dc6faa5263067f
Jared Hodgkins <jared.hodgkins@gmail.com>
2023-11-07 08:58:36 -0500

    109. default form values

--- 06-how-to-handle-forms/src/components/BookEdit.js
+++ 06-how-to-handle-forms/src/components/BookEdit.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function BookEdit() {
-  return <div>BookEdit</div>;
+function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title || '');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('New title:', title);
+  };
+
+  return (
+    <form className='book-edit' onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input
+        type='text'
+        className='input'
+        value={title}
+        onChange={handleChange}
+      />
+      <button className='button is-primary'>Save</button>
+    </form>
+  );
 }
 
 export default BookEdit;
