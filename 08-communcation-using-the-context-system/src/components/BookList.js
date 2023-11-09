c38704a8b4bad3775c0c9649a647651123e5b49b
Kavabux <jared.hodgkins@gmail.com>
2023-11-08 19:26:44 -0500

    131. context in action

--- 08-communcation-using-the-context-system/src/components/BookList.js
+++ 08-communcation-using-the-context-system/src/components/BookList.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
+  const value = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />
     );
   });
 
-  return <div className='book-list'>{renderedBooks}</div>;
+  return (
+    <div className='book-list'>
+      {value}
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
--- 08-communcation-using-the-context-system/src/index.js
+++ 08-communcation-using-the-context-system/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+import BooksContext from './context/books';
+
 import App from './App';
 
 const root = document.getElementById('root');
 
-ReactDOM.createRoot(root).render(<App />);
+ReactDOM.createRoot(root).render(
+  <BooksContext.Provider value={5}>
+    <App />
+  </BooksContext.Provider>
+);
