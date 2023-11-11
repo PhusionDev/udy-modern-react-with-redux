0423d9f540a5cc49f28ad4cabeade7250e298761
Kavabux <jared.hodgkins@gmail.com>
2023-11-10 20:16:46 -0500

    159. prop-types in action

--- 10-custom-navigation-and-routing-systems/src/Button.js
+++ 10-custom-navigation-and-routing-systems/src/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Button({
   children,
@@ -12,5 +13,22 @@ function Button({
 }) {
   return <button>{children}</button>;
 }
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        `You can only use one of the props primary, secondary, success, warning, danger.`
+      );
+    }
+  },
+};
 
 export default Button;
