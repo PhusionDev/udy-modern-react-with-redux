f882e67fe570e20b5901e1dc8e8e91575dc723b3
Kavabux <jared.hodgkins@gmail.com>
2023-11-13 20:18:29 -0500

    178. reminder on building lists

--- 10-custom-navigation-and-routing-systems/src/App.js
+++ 10-custom-navigation-and-routing-systems/src/App.js
@@ -3,11 +3,16 @@ import Accordion from './components/Accordion';
 function App() {
   const items = [
     {
+      id: '1231-21321',
       label: 'can I use react on a project',
       content: 'yes you can use react on a project',
     },
-    { label: 'javascript', content: 'hello america how are you' },
-    { label: 'css', content: 'sure thing css is good' },
+    {
+      id: '26243-233',
+      label: 'javascript',
+      content: 'hello america how are you',
+    },
+    { id: '383829-23992', label: 'css', content: 'sure thing css is good' },
   ];
 
   return <Accordion items={items} />;
