a13ccad29042c5152b92eae04fa89b44c734ef34
Kavabux <jared.hodgkins@gmail.com>
2023-11-13 20:14:59 -0500

    177. component setup

--- 10-custom-navigation-and-routing-systems/src/App.js
+++ 10-custom-navigation-and-routing-systems/src/App.js
@@ -1,48 +1,16 @@
-import React from 'react';
-import { GoBell, GoDownload, GoDatabase } from 'react-icons/go';
-
-import Button from './components/Button';
+import Accordion from './components/Accordion';
 
 function App() {
-  const handleClick = () => {
-    console.log('Clicked!');
-  };
+  const items = [
+    {
+      label: 'can I use react on a project',
+      content: 'yes you can use react on a project',
+    },
+    { label: 'javascript', content: 'hello america how are you' },
+    { label: 'css', content: 'sure thing css is good' },
+  ];
 
-  return (
-    <div>
-      <div>
-        <Button success rounded outline className='mb-5' onClick={handleClick}>
-          <GoBell />
-          Click Me!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoDownload />
-          Buy Now
-        </Button>
-      </div>
-      <div>
-        <Button danger>See Ads</Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoDatabase />
-          Hamburger
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Cheese
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Test 2
-        </Button>
-      </div>
-    </div>
-  );
+  return <Accordion items={items} />;
 }
 
 export default App;
